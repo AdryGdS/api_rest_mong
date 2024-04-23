4cb1d1a824f0c9aacf319e3d1246d624d63c1761
Adriely Gogora <99200566+AdryGdS@users.noreply.github.com>
2024-04-23 01:10:00 +0000

    Fim da aula pra mim

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.use(express.json());
 const contatoRouter = require('./routes/contato.Routes');
 app.use('/contatos', contatoRouter);
 
+
+app.use((req, res)=>{
+  res.statusCode = 400;
+  res.send({
+    error:"123",
+    message:"Rota inválida"
+  })
+})
+
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -24,3 +33,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+module.exports = app;
